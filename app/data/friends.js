78435bb91428e5dd0e016ce95ac5fcd5ae61f3c3
Shun Wakita <shun@hometeampm.local>
2018-04-09 11:58:24 -0700

    2nd changes

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Shun",
+		photo: "https://www.facebook.com/photo.php?fbid=10150096166823964&l=9e54201886",
+		scores: [5, 3, 4, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Philip Rivers",
+		photo: "http://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/5529.png&w=350&h=254",
+		scores: [5, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Tom Brady",
+		photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMz3QU39FeE8EXy-JSxVp-5EfDyCrafZJY-aHZbOhHNmCenrTz",
+		scores: [5, 2, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Steve Young",
+		photo: "http://www.profootballhof.com/assets/1/26/DimRelated/young-steve-3.jpg?54005",
+		scores: [4, 4, 4, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "LaDainian Tomlinson",
+		photo: "https://static01.nyt.com/images/2017/02/05/sports/05fame-web/05fame-web-master768.jpg",
+		scores: [4, 5, 5, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
